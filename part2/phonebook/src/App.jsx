2451b259d76cf00a61321ab52e6b4e0d5e224cf7
Jakob Schott <jakob.schott@gmail.com>
2026-02-04 18:59:56 +0100

    Working solution for part2 ex6

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,70 @@
+import { useState } from 'react'
+
+const Numbers = (props) => {
+	return (
+		<>
+			<h2>Numbers</h2>
+			{props.persons.map(person => 
+				<li key={person.id}>
+					{person.name}
+				</li>)}
+		</>
+	)
+}
+
+const AddContact = (props) => {
+
+	const addContactToPersons = (event) => {
+		event.preventDefault()
+
+		const contactObject = {
+			name: props.newName,
+			id: String(props.persons.length +1)
+		}
+
+		props.setPersons(props.persons.concat(contactObject))
+		props.setNewName('')
+	}
+	
+	const handleInputChange = (event) => {
+		console.log(event.target.value)
+		props.setNewName(event.target.value)
+	}
+
+	return (
+		<form onSubmit={addContactToPersons}>
+			<div>
+			name: <input
+					value={props.newName}
+					placeholder='John Doe'
+					onChange={handleInputChange}/>
+			</div>
+			<div>
+			<button 
+				type="submit"
+				>
+					add</button>
+			</div>
+      	</form>
+	)
+}
+
+const App = () => {
+  const [persons, setPersons] = useState([
+	{ 	
+		name: 'Arto Hellas', 
+		id: '0',
+	}
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <AddContact newName={newName} setNewName={setNewName} persons={persons} setPersons={setPersons}/>
+	  <Numbers persons={persons}/>
+    </div>
+  )
+}
+
+export default App
