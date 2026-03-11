f385338098af824740aa5fe663832e1c07a1835d
Jakob Schott <jakob@MacBook-Pro-von-Gruni.local>
2026-03-11 12:19:43 +0100

    created frontend build and updated project for web deplozment

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -117,7 +117,7 @@ app.post('/api/persons', (request, response) => {
 	response.json(newContact)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running port ${PORT}`)
 })
