2b9592fee4fe2ae5bf1f30d37ec7f67842c3be1c
Jakob Schott <154420406+jakobsitory@users.noreply.github.com>
2026-03-18 12:09:28 +0100

    part03ex12 added mongodb to phonebook

--- part3/phonebook-backend/mongo.js
+++ part3/phonebook-backend/mongo.js
@@ -1,33 +1,49 @@
 const mongoose = require('mongoose')
 
-if (process.argv.length < 3) {
+mongoose.set('strictQuery',false)
+const password = process.argv[2]
+const name = process.argv[3]
+const number = process.argv[4]
+
+if (!password) {
   console.log('give password as argument')
   process.exit(1)
 }
 
-const password = process.argv[2]
-
-const url = `mongodb+srv://fullstack:${password}@cluster0.vl1rfci.mongodb.net/testApp?retryWrites=true&w=majority&appName=Cluster0`
-
-// const url = `mongodb+srv://fullstack:${password}@cluster0.a5qfl.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0`
-
-mongoose.set('strictQuery',false)
-
+const url = `mongodb+srv://fullstack:${password}@cluster0.vl1rfci.mongodb.net/phonebook?retryWrites=true&w=majority&appName=Cluster0`
 mongoose.connect(url, { family: 4 })
-
-const noteSchema = new mongoose.Schema({
-  content: String,
-  important: Boolean,
+const contactSchema = new mongoose.Schema({
+  name: String,
+  number: String,
 })
+const Contact = mongoose.model('Contact', contactSchema)
 
-const Note = mongoose.model('Note', noteSchema)
+if (!name && !number) {
+  return Contact.find()
+    .then(result => {
+      result.forEach(contact => {
+        console.log(`${contact.name} ${contact.number} `)
+      })
+      mongoose.connection.close()
+      process.exit(1)
+    })
+}
 
-const note = new Note({
-  content: 'HTML is easy',
-  important: true,
-})
+if (name && number) {
+  // Create new contact
+  const contact = new Contact({
+    name: name,
+    number: number,
+  })
 
-note.save().then(result => {
-  console.log('note saved!')
-  mongoose.connection.close()
-})
+  return contact.save()
+  .then(result => {
+    console.log(`added ${name} number ${number} to phonebook`)
+    mongoose.connection.close()
+    process.exit(1)
+  })
+}
+
+console.log ('provide a name and number to create a new contact')
+mongoose.connection.close()
+process.exit(1)
