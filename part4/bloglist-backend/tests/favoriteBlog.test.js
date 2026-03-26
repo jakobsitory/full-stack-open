d81e7093d0534322a73890735fa908b2e9457604
Jakob Schott <154420406+jakobsitory@users.noreply.github.com>
2026-03-26 15:35:52 +0100

    part04ex05 working solution: test for favoriteBlog

---
+++ part4/bloglist-backend/tests/favoriteBlog.test.js
@@ -0,0 +1,29 @@
+const { test, describe } = require('node:test')
+const assert = require('node:assert')
+const favoriteBlog = require('../utils/list_helper').favoriteBlog
+const { blogs: createBlogs } = require('./mockList')
+
+
+describe('favorite blog', () => {
+    const blogs = createBlogs()
+    
+    test('of empty list is undefined', () => {
+        const result = favoriteBlog([])
+        assert.strictEqual(result, undefined)
+    })
+  
+    test('when list has only one blog, return the blog', () => {
+        const listWithOneBlog = [blogs[0]]
+
+        const result = favoriteBlog(listWithOneBlog)
+        assert.strictEqual(result, listWithOneBlog[0])
+    })
+    
+    test('a bigger list returns the favorite blog gets returned', () =>{
+        const favorite = blogs[2]
+
+        const result = favoriteBlog(blogs)
+        assert.strictEqual(result, favorite)
+
+    })
+})
--- part4/bloglist-backend/utils/list_helper.js
+++ part4/bloglist-backend/utils/list_helper.js
@@ -6,15 +6,26 @@ const totalLikes = (blogs) => {
     let likeCount = 0
 
     if (blogs.length > 0)
-        blogs.map(blog => {
-            console.log(blog.likes)
+        blogs.forEach(blog => {
             likeCount += blog.likes
         })
     
     return likeCount
 }
 
+const favoriteBlog = (blogs) => {
+    let favoriteBlog = blogs[0]
+
+    blogs.forEach(blog => {
+        if (blog.likes > favoriteBlog.likes) 
+            favoriteBlog = blog
+    })
+
+    return favoriteBlog
+}
+
 module.exports = {
     dummy,
     totalLikes,
+    favoriteBlog,
 }
