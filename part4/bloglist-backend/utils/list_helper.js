958f81137a850549a9dfd902d538fc7229c6c746
Jakob Schott <154420406+jakobsitory@users.noreply.github.com>
2026-03-26 15:01:36 +0100

    part04ex04 working solution: tests for lists of variable length

--- part4/bloglist-backend/utils/list_helper.js
+++ part4/bloglist-backend/utils/list_helper.js
@@ -1,7 +1,20 @@
 const dummy = (blogs) => {
- return 1
+    return 1
+}
+
+const totalLikes = (blogs) => {
+    let likeCount = 0
+
+    if (blogs.length > 0)
+        blogs.map(blog => {
+            console.log(blog.likes)
+            likeCount += blog.likes
+        })
+    
+    return likeCount
 }
 
 module.exports = {
-    dummy
+    dummy,
+    totalLikes,
 }
