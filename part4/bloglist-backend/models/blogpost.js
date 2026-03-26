0f872fe7bbcbb4c366dbdbc75663d16f386cf551
Jakob Schott <154420406+jakobsitory@users.noreply.github.com>
2026-03-26 11:22:31 +0100

    part04ex01 set up project strucure. Added blogpost schema

---
+++ part4/bloglist-backend/models/blogpost.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+
+const noteSchema = new mongoose.Schema({
+  content: {
+    type: String,
+    required: true,
+    minlength: 5,
+  },
+  important: Boolean,
+})
+
+const blogSchema = mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number,
+})
+
+blogSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
+})
+
+module.exports = mongoose.model('Note', noteSchema)
