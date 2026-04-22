f7dbc364f049615ec84b7fa71866ae9b242654c8
Jakob Schott <154420406+jakobsitory@users.noreply.github.com>
2026-04-22 11:31:03 +0200

    part05ex14 added test for Blog expansion

--- part5/bloglist-frontend/src/components/Blog.test.jsx
+++ part5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,9 +1,9 @@
 import { render, screen } from '@testing-library/react'
-// import userEvent from '@testing-library/user-event'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 
-test ('5.13 blog initially displays title and author', () => {
+describe('<Blog />', () => {
   const blog = {
     title: 'This is my blog title',
     author: 'This is my author',
@@ -14,43 +14,45 @@ test ('5.13 blog initially displays title and author', () => {
     }
   }
 
-  const user = {
+  const BlogUser = {
     id: '12345'
   }
 
-  render (
-    <Blog blog={blog} user={user}/>
-  )
+  beforeEach(() => {
+    render(
+      <Blog blog={blog} user={BlogUser}/>
+    )
+  })
 
-  const titleElement = screen.getByText(blog.title, { exact: false })
-  const authorElement = screen.getByText(blog.author, { exact: false })
+  test ('5.13 displays title and author initially', () => {
 
-  expect(titleElement).toBeVisible()
-  expect(authorElement).toBeVisible()
-})
 
-test ('5.13 blog initially does not display url and likes', () => {
-  const blog = {
-    title: 'This is my blog title',
-    author: 'This is my author',
-    url: 'www.blugurl.com',
-    likes: 5,
-    user: {
-      id: '12345'
-    }
-  }
+    const titleElement = screen.getByText(blog.title, { exact: false })
+    const authorElement = screen.getByText(blog.author, { exact: false })
 
-  const user = {
-    id: '12345'
-  }
+    expect(titleElement).toBeVisible()
+    expect(authorElement).toBeVisible()
+  })
 
-  render (
-    <Blog blog={blog} user={user}/>
-  )
+  test ('5.13 does not display url and likes initially', () => {
 
-  const urlElement = screen.getByText(blog.url, { exact: false })
-  const likesElement = screen.getByText(blog.likes, { exact: false })
+    const urlElement = screen.getByText(blog.url, { exact: false })
+    const likesElement = screen.getByText(blog.likes, { exact: false })
 
-  expect(urlElement).not.toBeVisible()
-  expect(likesElement).not.toBeVisible()
+    expect(urlElement).not.toBeVisible()
+    expect(likesElement).not.toBeVisible()
+  })
+
+  test ('5.14 displays url and likes after expand button is clicked', async () => {
+
+    const user = userEvent.setup()
+    const button = screen.getByRole('button')
+    await user.click(button)
+
+    const urlElement = screen.getByText(blog.url, { exact: false })
+    const likesElement = screen.getByText(blog.likes, { exact: false })
+
+    expect(urlElement).toBeVisible()
+    expect(likesElement).toBeVisible()
+  })
 })
