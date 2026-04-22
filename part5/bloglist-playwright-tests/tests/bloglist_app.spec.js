f0cf39d57de5fe84561024c34ba0a9963d46037d
Jakob Schott <154420406+jakobsitory@users.noreply.github.com>
2026-04-22 19:56:52 +0200

    part05ex19 added test for successfully adding a new blog

--- part5/bloglist-playwright-tests/tests/bloglist_app.spec.js
+++ part5/bloglist-playwright-tests/tests/bloglist_app.spec.js
@@ -1,5 +1,5 @@
 const { test, expect, describe, beforeEach } = require('@playwright/test')
-const { loginWith } = require('./helper')
+const { loginWith, createBlog } = require('./helper')
 
 describe('Bloglist App', () => {
   beforeEach(async ({ page, request }) => {
@@ -22,13 +22,12 @@ describe('Bloglist App', () => {
     await expect(page.getByLabel('username')).toBeVisible()
     await expect(page.getByLabel('password')).toBeVisible()
     await expect(page.getByRole('button', { name: 'login' })).toBeVisible()
-  })
+})
 
-
-  describe('Login', () => {
+describe('Login', () => {
     test('fails with wrong credentials', async ({ page }) => {
         await loginWith(page, 'testuser', 'wrong')
-
+        
         const errorDiv = page.locator('.error')
         await expect(errorDiv).toContainText('wrong credentials')
         await expect(errorDiv).toHaveCSS('border-style', 'solid')
@@ -39,13 +38,31 @@ describe('Bloglist App', () => {
     
     test('succeeds with correct credentials', async ({ page }) => {
         await loginWith(page, 'testuser', 'testpassword')
-        
-        const errorDiv = page.locator('.success')
-        await expect(errorDiv).toContainText('successfully logged in')
-        await expect(errorDiv).toHaveCSS('border-style', 'solid')
-        await expect(errorDiv).toHaveCSS('color', 'rgb(0, 128, 0)')
 
+        const successDiv = page.locator('.success')
+        await expect(successDiv).toContainText('successfully logged in')
+        await expect(successDiv).toHaveCSS('border-style', 'solid')
+        await expect(successDiv).toHaveCSS('color', 'rgb(0, 128, 0)')
+        
         await expect(page.getByText('Test User logged in')).toBeVisible()
+    })
+})
+
+describe('When logged in', () => {
+    beforeEach(async ({ page }) => {
+        await loginWith(page, 'testuser', 'testpassword')
+    })
+    
+    test('a new blog can be created', async ({ page }) => {
+        await createBlog(page, 'testblog', 'testauthor', 'testurl')
+
+        const successDiv = page.locator('.success')
+        await expect(successDiv).toContainText('Added new blog')
+        await expect(successDiv).toHaveCSS('border-style', 'solid')
+        await expect(successDiv).toHaveCSS('color', 'rgb(0, 128, 0)')
+
+        await expect(page.getByText(`testblog • testauthor`)).toBeVisible()
+        await expect(page.getByRole('button', { name: 'show' })).toBeVisible()
     })
   })
 })
