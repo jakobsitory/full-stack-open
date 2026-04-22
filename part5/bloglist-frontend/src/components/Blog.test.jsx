73d63b27467d7e8cd515906a3587ee6d389f91b8
Jakob Schott <154420406+jakobsitory@users.noreply.github.com>
2026-04-22 12:04:11 +0200

    part05ex15 added test for like button

--- part5/bloglist-frontend/src/components/Blog.test.jsx
+++ part5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,7 +1,18 @@
-import { render, screen } from '@testing-library/react'
+import { render, screen, waitFor } from '@testing-library/react'
+import { vi } from 'vitest'
 import userEvent from '@testing-library/user-event'
+import blogService from '../services/blogs'
 import Blog from './Blog'
 
+vi.mock('../services/blogs', () => ({
+  default: {
+    update: vi.fn(),
+    remove: vi.fn(),
+    create: vi.fn(),
+    getAll: vi.fn(),
+    setToken: vi.fn(),
+  },
+}))
 
 describe('<Blog />', () => {
   const blog = {
@@ -18,26 +29,29 @@ describe('<Blog />', () => {
     id: '12345'
   }
 
+  const setBlogs = vi.fn()
+
   beforeEach(() => {
+    vi.clearAllMocks()
     render(
-      <Blog blog={blog} user={BlogUser}/>
+      <Blog blog={blog} user={BlogUser} setBlogs={setBlogs}/>
     )
   })
 
-  test ('5.13 displays title and author initially', () => {
+  test ('5.13 displays title and author initially', async () => {
 
 
-    const titleElement = screen.getByText(blog.title, { exact: false })
-    const authorElement = screen.getByText(blog.author, { exact: false })
+    const titleElement = await screen.getByText(blog.title, { exact: false })
+    const authorElement = await screen.getByText(blog.author, { exact: false })
 
     expect(titleElement).toBeVisible()
     expect(authorElement).toBeVisible()
   })
 
-  test ('5.13 does not display url and likes initially', () => {
+  test ('5.13 does not display url and likes initially', async () => {
 
-    const urlElement = screen.getByText(blog.url, { exact: false })
-    const likesElement = screen.getByText(blog.likes, { exact: false })
+    const urlElement = await screen.getByText(blog.url, { exact: false })
+    const likesElement = await screen.getByText(blog.likes, { exact: false })
 
     expect(urlElement).not.toBeVisible()
     expect(likesElement).not.toBeVisible()
@@ -49,10 +63,21 @@ describe('<Blog />', () => {
     const button = screen.getByRole('button')
     await user.click(button)
 
-    const urlElement = screen.getByText(blog.url, { exact: false })
-    const likesElement = screen.getByText(blog.likes, { exact: false })
-
+    const urlElement = await screen.getByText(blog.url, { exact: false })
+    const likesElement = await screen.getByText(blog.likes, { exact: false })
+    
     expect(urlElement).toBeVisible()
     expect(likesElement).toBeVisible()
+})
+
+test('5.14 clicking like button triggers a function', async () => {
+    
+    const user = userEvent.setup()
+    
+    const likeButtonElement = screen.getByText('like')
+    await user.click(likeButtonElement)
+    await user.click(likeButtonElement)
+
+    expect(setBlogs.mock.calls).toHaveLength(2)
   })
 })
