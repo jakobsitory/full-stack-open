0ab32f2ab3f67895358c85b4029b2311da3de525
Jakob Schott <154420406+jakobsitory@users.noreply.github.com>
2026-04-29 15:58:52 +0200

    part05ex29 added MUI component Alert for notifications

--- part5/bloglist-frontend/src/components/Notification.jsx
+++ part5/bloglist-frontend/src/components/Notification.jsx
@@ -1,12 +1,6 @@
+import Alert from '@mui/material/Alert'
+
 const Notification = ({ content, setNotificationMessage }) => {
-  const notificationStyle = {
-    background: 'lightgrey',
-    fontSize: '20px',
-    borderStyle: 'solid',
-    borderRadius: '5px',
-    padding: '10px',
-    marginBottom: '10px',
-  }
 
   if (content.show === false) {
     return null
@@ -15,9 +9,9 @@ const Notification = ({ content, setNotificationMessage }) => {
   setTimeout(() => { setNotificationMessage({ show: false }) }, 4000)
 
   return (
-    <div style={notificationStyle} className={content.type}>
+    <Alert severity={content.type}>
       {content.message}
-    </div>
+    </Alert>
   )
 }
 
